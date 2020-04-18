6127b454177ce6660c6dec0f885636f0086ccd0b
Steve Sharman <steve@hackthorn.ai>
2020-04-19 00:25:03 +0100

    Updated standard library version

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [com.cognitect.aws/endpoints "1.1.11.753"]
                  [com.cognitect.aws/sns "773.2.578.0"]
                  [com.cognitect.aws/ssm "794.2.640.0"]
-                 [synergy-events-stdlib "0.1.3"]]
+                 [synergy-events-stdlib "0.1.7"]]
   :repl-options {:init-ns synergy-test-normaliser.core}
   :profiles {:uberjar {:aot :all}}
   :uberjar-name "synergy-test-normaliser.jar")
