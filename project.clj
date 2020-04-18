23f75366f406f3fba27459d891a45cfe6e2c1e51
Steve Sharman <steve@hackthorn.ai>
2020-04-18 19:42:29 +0100

    Updated to use the synergy events standard library

--- project.clj
+++ project.clj
@@ -12,7 +12,8 @@
                  [com.cognitect.aws/api "0.8.456"]
                  [com.cognitect.aws/endpoints "1.1.11.753"]
                  [com.cognitect.aws/sns "773.2.578.0"]
-                 [com.cognitect.aws/ssm "794.2.640.0"]]
+                 [com.cognitect.aws/ssm "794.2.640.0"]
+                 [synergy-events-stdlib "0.1.3"]]
   :repl-options {:init-ns synergy-test-normaliser.core}
   :profiles {:uberjar {:aot :all}}
   :uberjar-name "synergy-test-normaliser.jar")
--- src/synergy_test_normaliser/core.clj
+++ src/synergy_test_normaliser/core.clj
@@ -5,6 +5,7 @@
             [cognitect.aws.client.api :as aws]
             [synergy-specs.events :as synspec]
             [clojure.spec.alpha :as s]
+            [synergy-events-stdlib.core :as stdlib]
             [taoensso.timbre :as timbre
              :refer [log trace debug info warn error fatal report
                      logf tracef debugf infof warnf errorf fatalf reportf
@@ -15,111 +16,11 @@
 
 (def ssm (aws/client {:api :ssm}))
 
-(def routeTableParameters {
-                           :arn-prefix "synergyDispatchTopicArnRoot"
-                           :event-store-topic "synergyEventStoreTopic"
-                           })
-
 (def snsArnPrefix (atom ""))
 
 (def eventStoreTopic (atom ""))
 
-(defn getRouteTableParametersFromSSM []
-  "Look up values in the SSM parameter store to be later used by the routing table"
-  (let [snsPrefix (get-in (aws/invoke ssm {:op :GetParameter
-                                           :request {:Name (get routeTableParameters :arn-prefix)}})
-                          [:Parameter :Value])
-        evStoreTopic (get-in (aws/invoke ssm {:op :GetParameter
-                                              :request {:Name (get routeTableParameters :event-store-topic)}})
-                             [:Parameter :Value])
-        ]
-    ;; //TODO: add error handling so if for any reason we can't get the values, this is noted
-    {:snsPrefix snsPrefix :eventStoreTopic evStoreTopic}))
-
-(defn setEventStoreTopic [parameter-map]
-  "Set the eventStoreTopic atom with the required value"
-  (swap! eventStoreTopic str (get parameter-map :eventStoreTopic)))
-
-(defn setArnPrefix [parameter-map]
-  "Set the snsArnPrefix atom with the required value"
-  (swap! snsArnPrefix str (get parameter-map :snsPrefix)))
-
 (def deliveryTopic "testInputTopic")
-
-(defn gen-status-map
-  "Generate a status map from the values provided"
-  [status-code status-message return-value]
-  (let [return-status-map {:status status-code :description status-message :return-value return-value}]
-    return-status-map))
-
-(defn set-up-route-table []
-  (reset! snsArnPrefix "")
-  (reset! eventStoreTopic "")
-  (info "Routing table not found - setting up (probably first run for this Lambda instance")
-  (let [route-paraneters (getRouteTableParametersFromSSM)]
-    (setArnPrefix route-paraneters)
-    (setEventStoreTopic route-paraneters)))
-
-(defn send-to-topic
-  ([thisTopic thisEvent]
-   (send-to-topic thisTopic thisEvent ""))
-  ([topic event note]
-   (let [thisEventId (get event ::synspec/eventId)
-         jsonEvent (json/write-str event)
-         eventSNS (str @snsArnPrefix topic)
-         snsSendResult (aws/invoke sns {:op :Publish :request {:TopicArn eventSNS
-                                                               :Message  jsonEvent}})]
-     (if (nil? (get snsSendResult :MessageId))
-       (do
-         (info "Error dispatching event to topic : " topic " (" note ") : " event)
-         (gen-status-map false "error-dispatching-to-topic" {:eventId thisEventId
-                                                             :error snsSendResult}))
-       (do
-         (info "Dispatching event to topic : " eventSNS " (" note ") : " event)
-         (gen-status-map true "dispatched-to-topic" {:eventId   thisEventId
-                                                     :messageId (get snsSendResult :MessageId)}))))))
-
-(defn validate-message [inbound-message]
-  (if (s/valid? ::synspec/synergyEvent inbound-message)
-    (gen-status-map true "valid-inbound-message" {})
-    (gen-status-map false "invalid-inbound-message" (s/explain-data ::synspec/synergyEvent inbound-message))))
-
-(defn check-event-type [event]
-  "Deduce what type of AWS event triggered the Lambda, based on message field content"
-  (let [checkRecordType (get event :Records)
-        evCheck1 (get (first (get event :Records)) :eventSource)
-        evCheck2 (get (first (get event :Records)) :EventSource)
-        srcCheck (get event :source)
-        routeKeyCheck (get event :routeKey)
-        ]
-    (cond (and (not (nil? checkRecordType)) (= evCheck1 "aws:sqs"))
-          "SQS"
-          (and (not (nil? checkRecordType)) (= evCheck1 "aws:s3"))
-          "S3"
-          (and (not (nil? checkRecordType)) (= evCheck2 "aws:sns"))
-          "SNS"
-          (and (nil? checkRecordType) (= srcCheck "aws.events"))
-          "Cloudwatch"
-          (and (nil? checkRecordType) (not (nil? routeKeyCheck)))
-          "APIGateway"
-          :else
-          "NOTKNOWN")))
-
-(defn get-event-data [event src-type]
-  "Extract the data from an inbound message based on the event type"
-  (cond (= src-type "SNS")
-        (get (get (first (get event :Records)) :Sns) :Message)
-        (= src-type "SQS")
-        (get (first (get event :Records)) :body)
-        (= src-type "S3")
-        (first (get event :Records))
-        (= src-type "Cloudwatch")
-        event
-        (= src-type "APIGateway")
-        {:routeKey (get event :routeKey)
-         :body (get event :body)}
-        :else
-        nil))
 
 ;; Event specific processing
 
@@ -140,21 +41,21 @@
 
 (defn process-event [event-content event-type]
   (if (empty? @snsArnPrefix)
-    (set-up-route-table))
+    (stdlib/set-up-topic-table snsArnPrefix eventStoreTopic ssm))
   (let [jevent (json/read-str event-content :key-fn keyword)
         tevent (generate-new-event jevent)
         wevent (synspec/wrap-std-event tevent)]
     (info "JEVENT : " jevent)
     (info "TEVENT : " tevent)
     (info "WEVENT : " wevent)
-    (if (true? (get (validate-message wevent) :status))
-      (send-to-topic deliveryTopic tevent)
+    (if (true? (get (stdlib/validate-message wevent) :status))
+      (stdlib/send-to-topic deliveryTopic tevent @snsArnPrefix sns)
       (info "VALIDATION FAILED"))))
 
 (defn handle-event [event]
   (info "Raw event: " (print-str event))
-  (let [deduced-type (check-event-type event)
-        event-content (get-event-data event deduced-type)]
+  (let [deduced-type (stdlib/check-event-type event)
+        event-content (stdlib/get-event-data event deduced-type)]
   (process-event event-content deduced-type)))
 
 
